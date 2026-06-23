9b89b40cacc047c8578369f61e38f10b8dcabc69
Rodrigo Sanchez <sanxcruro122@gmail.com>
2026-06-22 23:29:03 -0600

    refactor: consolidar proyecto Cargo en la raiz y remover hello-world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
+    //Hola mundo
     println!("Hola, Rust!");
+
+    //Variables
+    let mut my_string: &str = "Esto es una cadena de texto";
+    println!("{my_string}"); //Imprime la primer declaracion
+    my_string = "Aqui se cambio el valor de la cadena de texto";
+    println!("{my_string}"); //Implrime el cambio 
 }
