53c9bb592237329c39451d75cbbd8f03cd0641e1
Rodrigo Sanchez <sanxcruro122@gmail.com>
2026-06-23 10:24:36 -0600

    feat: implementar ejemplos básicos de manipulación de variables y strings en main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,18 @@ fn main() {
     println!("{my_string}"); //Imprime la primer declaracion
     my_string = "Aqui se cambio el valor de la cadena de texto";
     println!("{my_string}"); //Implrime el cambio 
-    my_string ="Prueba de mutacion";
+    my_string = "Prueba de mutacion";
     println!("{my_string}");
 
-    let my_string2 : String = String :: from("Esta es otra cadena de texto"); //Aqui solo designamos una cantidad limitada de memoria
+    let my_string2: String = String::from("Esta es otra cadena de texto"); //Aqui solo designamos una cantidad limitada de memoria
     println!("{my_string2}");
+
+    let mut my_int: i32 = 7; //Integer 
+    my_int = my_int + 4;
+    println!("{}", my_int - 1); // Usamos la mutacion de la variable en ejecucion
+
+    println!("Este es el valor de la variable my_int: {}", my_int);
+
+    let my_int64: i64 = 7;
+    println!("{my_int64}");
 }
